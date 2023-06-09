fccf7f1fa18665937df57cc781de291f5392f709
JoelOniha <134166393+JoelOniha@users.noreply.github.com>
2023-06-09 02:11:24 +0200

    Day 9 homework

---
+++ week2/Day9/task-list/src/components/task/TaskPage.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { Task } from '../../models/Task';
+import TaskInput from './TaskInput';
+import TaskTable from './TaskTable';
+
+import TaskService from '../../services/task-service';
+
+export default function TaskPage() {
+    const [tasks, setTasks] = useState([]);
+
+  useEffect(() =>{
+    if(!tasks.length){
+      onInitialLoad();
+    }
+  },
+  
+  
+  );
+
+  async function onInitialLoad() {
+    try{
+    const tasks = await TaskService.fetchTasks();
+    setTasks(tasks); 
+  }catch(err){
+  }
+}
+
+  async function onTaskCreate(name) {
+    const task = await TaskService.createTask(new Task(null, name, false));
+    setTasks([...tasks, task]);
+  }
+
+  async function onTaskRemove(taskId) {
+    await TaskService.deleteTask(taskId);
+
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  async function onTaskCompleteToggle(taskId) {
+    const taskToToggle = tasks.find((task) => task.id === taskId);
+    taskToToggle.complete = !taskToToggle.complete;
+
+    await TaskService.updateTask(taskToToggle);
+
+    setTasks(tasks.map((task) => {
+      return task.id === taskId ? taskToToggle : task;
+    }));
+  }
+
+  return (
+    <div className="container mt-5 text-center">
+      <div className="card card-body text-center">
+        <h1>Task List</h1>
+        <hr></hr>
+        <p>Our Firebase Task List</p>
+        <TaskInput onTaskCreate={onTaskCreate}></TaskInput>
+        <TaskTable
+          tasks={tasks}
+          onTaskRemove={onTaskRemove}
+          onTaskCompleteToggle={onTaskCompleteToggle}
+        ></TaskTable>
+      </div>
+    </div>
+  )
+}
